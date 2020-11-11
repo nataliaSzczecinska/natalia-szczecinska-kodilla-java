804fe8e86203e75ed7475ae11c83073e03fb904b
nataliaSzczecinska <nataliaszczecinska@o2.pl>
2020-11-11 22:30:48 +0100

    6.2. pierwszy test jednostkowy

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,39 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUserName();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        //Test kalkulatora
+        Calculator calculator = new Calculator();
+
+        int calculatorResults = calculator.add(5, 4);
+
+        if (calculatorResults == (5 + 4)){
+            System.out.println("Adding works fine");
+        } else {
+            System.out.println("Error! Adding does not work fine");
+        }
+
+        calculatorResults = calculator.subtract(5, 4);
+
+        if (calculatorResults == (5 - 4)){
+            System.out.println("Subtraction works fine");
+        } else {
+            System.out.println("Error! Subtraction does not work fine");
+        }
     }
 }
