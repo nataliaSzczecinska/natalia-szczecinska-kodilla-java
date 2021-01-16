a7759fb478fe728635869e3459f96915af586301
nataliaSzczecinska <nataliaszczecinska@o2.pl>
2021-01-16 15:44:42 +0100

    14.1 spring intro

---
+++ kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/SpringIntroRunner.java
@@ -0,0 +1,7 @@
+package com.kodilla.spring.intro;
+
+public class SpringIntroRunner {
+    public static void main (String [] arg) {
+        System.out.println("Welcome into the intro to spring");
+    }
+}
