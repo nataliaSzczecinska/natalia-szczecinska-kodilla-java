16bc936dc5fea5fd5fe1f1ee169879ee25c0bd1f
nataliaSzczecinska <nataliaszczecinska@o2.pl>
2021-01-25 10:36:57 +0100

    15.6 Debugger Exercise

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/loop/TestLoopAndConditionalBreakpoints.java
@@ -0,0 +1,23 @@
+package com.kodilla.testing.loop;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestLoopAndConditionalBreakpoints {
+
+    @Test
+    public void testLoop() {
+        //Given
+        long sum = 0;
+
+        //When
+        for (int n = 0 ; n < 1000 ; n++) {
+            sum += n;
+            System.out.println("[" + n + "] sum equals: " + sum);
+        }
+
+        //Then
+        Assert.assertEquals(499500, sum);
+    }
+}
