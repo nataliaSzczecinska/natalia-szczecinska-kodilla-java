b230eb42115696e4bcfd5a078c62bbe8e60c2652
nataliaSzczecinska <nataliaszczecinska@o2.pl>
2021-02-11 10:29:33 +0100

    17.4 - nativeQuery problem

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/dao/TaskDao.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/dao/TaskDao.java
@@ -1,5 +1,6 @@
 package com.kodilla.hibernate.task.dao;
 
+import org.springframework.data.jdbc.repository.query.*;
 import org.springframework.stereotype.Repository;
 import javax.transaction.Transactional;
 import com.kodilla.hibernate.task.Task;
@@ -10,4 +11,13 @@ import java.util.*;
 @Repository
 public interface TaskDao extends CrudRepository<Task, Integer> {
     List<Task> findByDuration(int duration);
+
+    @Query
+    List<Task> retrieveLongTasks();
+
+    @Query
+    List<Task> retrieveShortTasks();
+
+    @Query(nativeQuery = true)
+    List<Task> retrieveTasksWithEnoughTime();
 }
