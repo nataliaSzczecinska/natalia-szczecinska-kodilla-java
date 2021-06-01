29a65a6e87c1fd113ac29876836ca947e53bc8e7
nataliaSzczecinska <nataliaszczecinska@o2.pl>
2021-06-01 08:58:57 +0200

    Task 31.2 fb correct

--- kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -8,14 +8,24 @@ import org.openqa.selenium.support.ui.Select;
 
 public class FacebookTestingApp {
     public static final String FACEBOOK_URL = "https://www.facebook.com/";
-    public static final String DAY_OF_BIRTH = "//div[contains(@class, \"5k_5\")]/span/span/select[@id = \"day\"]";
-    public static final String MONTH_OF_BIRTH = "//div[contains(@class, \"5k_5\")]/span/span/select[@id = \"month\"]";
-    public static final String YEAR_OF_BIRTH = "//div[contains(@class, \"5k_5\")]/span/span/select[@id = \"year\"]";
+    public static final String FACEBOOK_COOKIES_ACCEPT = "//*[@data-testid='cookie-policy-dialog-accept-button']";
+    public static final String FACEBOOK_REGISTER = "//*[@data-testid='open-registration-form-button']";
+    public static final String DAY_OF_BIRTH = "//*[@id=\"day\"]";
+    public static final String MONTH_OF_BIRTH = "//*[@id=\"month\"]";
+    public static final String YEAR_OF_BIRTH = "//*[@id=\"year\"]";
 
 
     public static void main(String[] args) throws InterruptedException {
         WebDriver webDriver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
         webDriver.get(FACEBOOK_URL);
+
+        WebElement cookies = webDriver.findElement(By.xpath(FACEBOOK_COOKIES_ACCEPT));
+        cookies.click();
+        Thread.sleep(2000);
+
+        WebElement register = webDriver.findElement(By.xpath(FACEBOOK_REGISTER));
+        register.click();
+        Thread.sleep(2000);
 
         WebElement dayOfBirth = webDriver.findElement(By.xpath(DAY_OF_BIRTH));
         Select selectDay = new Select(dayOfBirth);
